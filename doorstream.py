c6e9061e9f661cd532ac21970291fe44a7720dba
Retrontology <retrontology@hotmail.com>
2021-10-08 15:11:01 -0500

    mjpg streaming

--- doorstream.py
+++ doorstream.py
@@ -1,17 +1,36 @@
 import cv2
 import numpy as np
-from http.server import BaseHTTPRequestHandler,HTTPServer
+from http.server import BaseHTTPRequestHandler
+from doorcam import *
 
+class MJPGStream(BaseHTTPRequestHandler):
 
-
-class Stream(BaseHTTPRequestHandler):
+    def __init__(self, camera: Camera, *args, **kwargs):
+        self.camera = camera
+        super().__init__(*args, **kwargs)
 
     def do_GET(self):
         
-        if self.path == '/stream':
+        if self.path == '/stream.mjpg':
+
             self.send_response(200)
-            self.send_header(
-                'Content-type',
-                'multipart/x-mixed-replace; boundary=--jpgboundary'
-            )
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+
+            while True:
+                ret, image = cv2.imencode('.jpg', self.camera.get_current_frame())
+                if ret:
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-type', 'image/jpeg')
+                    self.send_header('Content-length', str(image.size))
+                    self.end_headers()
+                    self.wfile.write(image.tostring())
+                    self.wfile.write(b'\r\n')
+                else:
+                    continue
+        else:
+            self.send_error(404)
             self.end_headers()
--- run.py
+++ run.py
@@ -5,13 +5,21 @@ from doorscreen import *
 from doorcam import *
 from doorstream import *
 from time import sleep
+from http.server import HTTPServer
+from functools import partial
 
 def main():
-    cam = Camera(rotation=cv2.ROTATE_90_CLOCKWISE, convert_rgb=False)
-    screen = Screen()
+    cam = Camera()
+    screen = Screen(rotation=cv2.ROTATE_90_CLOCKWISE)
+    stream_handler = partial(MJPGStream, cam)
+    server = HTTPServer(('0.0.0.0', 8080), stream_handler)
+    http_thread = Thread(target=server.serve_forever)
+    http_thread.start()
     while True:
-        print(cam.fps)
-        sleep(1)
+        try:
+            screen.fb_write_image(cam.get_current_frame())
+        except:
+            pass
         
 
 if __name__ == '__main__':
